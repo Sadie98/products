6de1ea876c4b21efd8811c22b8f10adf5c768a4d
Sadie <avdeewa.ul@yandex.ru>
2020-12-19 19:02:37 +0300

    Добавлена обработка кнопок на домашнюю страницу

--- pages/home.php
+++ pages/home.php
@@ -2,8 +2,18 @@
 <script type="application/javascript">
     $(function(){
         $('.body').append('<div class="button-container">' +
-            '<button class="button button-blue">Посмотреть товары</button> ' +
-            '<button class="button button-pink right-button">Добавить товары</button> ' +
-            '</div>')
+            '<button class="button button-blue see-products">Посмотреть товары</button> ' +
+            '<button class="button button-pink right-button add-product">Добавить товар</button> ' +
+            '</div>');
+
+        $('.see-products').click(() => {
+            const url = document.location.href.split('?')[0] + "?page=all";
+            document.location = url;
+        });
+
+        $('.add-product').click(() => {
+            const url = document.location.href.split('?')[0] + "?page=add";
+            document.location = url;
+        });
     });
 </script>
