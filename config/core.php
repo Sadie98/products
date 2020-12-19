15b959194b12d94db8856b83ef2e00e7372604d8
Sadie <avdeewa.ul@yandex.ru>
2020-12-19 16:17:10 +0300

    Добавлен шаблон для сайта, подключение к базе

---
+++ config/core.php
@@ -0,0 +1,15 @@
+<?php
+
+$username = "u228065_sadie";
+$password = "avdeeva98";
+$dbname = "b228065_products";
+$host = "78.108.80.33";
+
+try {
+    $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    echo "Connected to $dbname at $host successfully.";
+} catch (PDOException $pe) {
+    die("Could not connect to the database $dbname :" . $pe->getMessage());
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+require_once("config/core.php");
+
+switch ($_GET['option']) {
+    case "page":
+        include("pages/page.php");
+        break;
+    default:
+        include("pages/home.php");
+        break;
+}
+
+include ("template.php");
